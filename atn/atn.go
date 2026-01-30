2a95a3f5c2df1c0605325ed855c30bfb6544df44
Markus Rudolph <markus.rudolph@typefox.io>
2026-01-30 11:16:24 +0100

    Add rule start and stop maps
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

--- atn/atn.go
+++ atn/atn.go
@@ -1,18 +1,19 @@
 package atn
 
+import "typefox.dev/fastbelt/internal/grammar/generated"
+
 type ATN interface {
 	States() []*ATNState
+	RuleStartState(rule *generated.ParserRule) *RuleStartState
+	RuleStopState(rule *generated.ParserRule) *RuleStopState
 	AddState(state ATNState)
+	AddStartAndStopState(rule *generated.ParserRule, start *RuleStartState, stop *RuleStopState)
 }
 
 type ATNData struct {
-	states []*ATNState
-}
-
-func NewATN() ATN {
-	return &ATNData{
-		states: []*ATNState{},
-	}
+	states           []*ATNState
+	ruleToStartState map[*generated.ParserRule]*RuleStartState
+	ruleToStopState  map[*generated.ParserRule]*RuleStopState
 }
 
 func (a *ATNData) States() []*ATNState {
@@ -21,4 +22,31 @@ func (a *ATNData) States() []*ATNState {
 
 func (a *ATNData) AddState(state ATNState) {
 	a.states = append(a.states, &state)
+}
+
+func (a *ATNData) AddStartAndStopState(rule *generated.ParserRule, start *RuleStartState, stop *RuleStopState) {
+	a.ruleToStartState[rule] = start
+	a.ruleToStopState[rule] = stop
+}
+
+func (a *ATNData) RuleStartState(rule *generated.ParserRule) *RuleStartState {
+	return a.ruleToStartState[rule]
+}
+
+func (a *ATNData) RuleStopState(rule *generated.ParserRule) *RuleStopState {
+	return a.ruleToStopState[rule]
+}
+
+func NewATN() ATN {
+	return &ATNData{
+		states:           []*ATNState{},
+		ruleToStartState: make(map[*generated.ParserRule]*RuleStartState),
+		ruleToStopState:  make(map[*generated.ParserRule]*RuleStopState),
+	}
+}
+
+func NewATNFromGrammar(grammar generated.Grammar) *ATN {
+	atnBuilder := NewATNBuilder()
+	atnBuilder.InititializeStartAndStopStates(grammar.Rules())
+	return atnBuilder.Build()
 }
