f6f675f7e4344b818a27e7d0bb168da584093acf
Markus Rudolph <markus.rudolph@typefox.io>
2026-09-10 15:36:37 +0200

    Add mustExpandInfixRules
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

--- internal/generator/lexer_util.go
+++ internal/generator/lexer_util.go
@@ -194,6 +194,7 @@ func (r GenerateTokenTypesResult) TokenTypeNamesByTokenIndex() []string {
 }
 
 func GenerateTokenTypes(grammr grammar.Grammar) GenerateTokenTypesResult {
+	mustExpandInfixRules(grammr)
 	keywords := GetAllKeywords(grammr)
 	tokenDecls := GetAllTokenDecls(grammr)
 	tokenGroups := GetAllTokenGroups(grammr)
